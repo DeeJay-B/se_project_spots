0a58be492298ef658057d09e21402f9b25f83b56
Deejay <dbell0308@gmail.com>
2024-08-20 10:37:24 -0700

    spots project update modal

--- scripts/index.js
+++ scripts/index.js
@@ -26,3 +26,21 @@ const initialCards = [
 ];
 
 console.log(initialCards);
+
+const profileEditButton = document.querySelector(".profile__edit-button");
+
+const editModal = document.querySelector("#edit-profile-modal");
+
+const editModalCloseBtn = editModal.querySelector(".modal__close-btn");
+
+function openModal() {
+  editModal.classList.add("modal_opened");
+}
+
+function closeModal() {
+  editModal.classList.remove("modal_opened");
+}
+
+profileEditButton.addEventListener("click", openModal);
+
+editModalCloseBtn.addEventListener("click", closeModal);
